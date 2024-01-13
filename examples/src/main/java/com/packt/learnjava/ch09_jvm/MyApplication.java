794bba378dcb5764ed5a08a4a8a9ea32997b5468
Nick Samoylov <nsamoylov@hotmail.com>
2024-01-13 09:36:08 -0700

    Add reading env variable example

--- examples/src/main/java/com/packt/learnjava/ch09_jvm/MyApplication.java
+++ examples/src/main/java/com/packt/learnjava/ch09_jvm/MyApplication.java
@@ -8,8 +8,11 @@ public class MyApplication {
         for(String arg: args){
             System.out.print(arg + " ");     //prints all program arguments
         }
+        String e = System.getenv("someVariable");
+        System.out.println(e);               //prints value of environment variable someVariable
+
         String p = System.getProperty("someParameter");
-        System.out.println("\n" + p);               //prints value of VM option someParameter
+        System.out.println("\n" + p);        //prints value of VM option someParameter
 
         int i = ExampleClass.multiplyByTwo(2);
         System.out.println(i);      //prints: 4
