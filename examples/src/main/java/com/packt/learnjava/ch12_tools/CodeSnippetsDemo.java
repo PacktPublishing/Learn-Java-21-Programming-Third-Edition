ba35dc08a65c0902888d54d916051a6d20e539b7
Nick Samoylov <nsamoylov@hotmail.com>
2024-03-10 10:25:07 -0600

    Add CodeSnippetDemo2

--- examples/src/main/java/com/packt/learnjava/ch12_tools/CodeSnippetsDemo.java
+++ examples/src/main/java/com/packt/learnjava/ch12_tools/CodeSnippetsDemo.java
@@ -1,30 +1,24 @@
 package com.packt.learnjava.ch12_tools;
-/** This class shows example of the code snippets
-   embedded in the API documentation. For example, class usage:
- {@snippet :
-  * // @highlight substring="cs" region="example1"
-  * var cs = new CodeSnippetsDemo();
-  * cs.method();
-  * // @end region="example1"
-  *}
- And here is a reference to another class doc
- {@snippet class="CodeSnippets"}
-
- or we can refer another code region only
- {@snippet class=CodeSnippets region=snippet1}
-
- It is possible to refer to non-code file, too.
- For example, imagine we want to show certain properties
- used by this class as follows:
- {@snippet file='demo.properties' region=prop1}
+/**
+ * This is class that shows snippets usage
+ * Use the following command to generate API docs from the examples directory:
+ * javadoc -d ~/snippets src/main/java/com/packt/learnjava/ch12_tools/CodeSnippetsDemo.java
  */
 public class CodeSnippetsDemo {
     /**
+     * This is default constructor
+     */
+    public CodeSnippetsDemo(){}
+    /**
      * This is example of a code snippet embedded in the method description:
-     *
-     * {@snippet :
+     * {@snippet lang="java" :
      * CodeSnippetsDemo codeSnippetsDemo = new CodeSnippetsDemo();
-     * codeSnippetsDemo.method();
+     * codeSnippetsDemo.method();  // @highlight substring="method()" type="highlighted"
+     *}
+     * And this is an example of properties snippet:
+     * {@snippet lang=properties :
+     * property1=some string
+     * property2=42
      *}
      */
     public void method(){
---
+++ examples/src/main/java/com/packt/learnjava/ch12_tools/CodeSnippetsDemo2.java
@@ -0,0 +1,40 @@
+package com.packt.learnjava.ch12_tools;
+/** This class shows example of the code snippets
+   embedded in the API documentation. For example, class usage:
+ {@snippet :
+  * // @highlight substring="cs" region="example1"
+  * var cs = new CodeSnippetsDemo();
+  * cs.method();
+  * // @end region="example1"
+  *}
+ And here is a reference to another class doc
+ {@snippet class="CodeSnippets"}
+
+ or we can refer another code region only
+ {@snippet class=CodeSnippets region=snippet1}
+
+ It is possible to refer to non-code file, too.
+ For example, imagine we want to show certain properties
+ used by this class as follows:
+ {@snippet file='demo.properties' region=prop1}
+ */
+public class CodeSnippetsDemo2 {
+    /**
+     * This is default constructor
+     */
+    public CodeSnippetsDemo2(){}
+    /**
+     * This is example of a code snippet embedded in the method description:
+     * {@snippet lang = "java":
+     * CodeSnippetsDemo codeSnippetsDemo = new CodeSnippetsDemo();
+     * codeSnippetsDemo.method();  // @highlight substring="method()"
+     *}
+     * And this is an example of properties snippet:
+     * {@snippet lang=properties :
+     * property1=some string
+     * property2=42
+     *}
+     */
+    public void method(){
+    }
+}
